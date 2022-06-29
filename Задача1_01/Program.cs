bfc9c2d6faf1f5437749cb1400d0c86e84e82ed7
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-29 14:37:04 +0500

    дз на семинар 3

---
+++ Задача1_01/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+void FindCoord(int N)
+{
+    if (N==1) Console.WriteLine("X>0,Y>0");
+    if (N==2) Console.WriteLine("X<0,Y>0");
+    if (N==3) Console.WriteLine("X<0,Y<0");
+    if (N==4) Console.WriteLine("X>0,Y<0");
+    if (N<1 || N>4) Console.WriteLine("Такой четверти не существует");
+}
+Console.WriteLine("Введите номер четверти ");
+int n = Convert.ToInt32(Console.ReadLine());
+FindCoord(n);
---
+++ Задача_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт квадраты чисел от 1 до N
+
+void sqrtN (int N)
+{
+    int Count = 1;
+    while (Count <= N)
+    {
+        int sqrt = Count*Count;
+        Console.WriteLine(sqrt);//ecли WriteLine, то Console.WriteLine(sqrt+ "")
+        Count++;
+    }
+}
+Console.Write("Input Digit: ");
+int n = Convert.ToInt32(Console.ReadLine());
+sqrtN(n);
